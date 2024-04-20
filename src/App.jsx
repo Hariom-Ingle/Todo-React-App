72129c3a38a550015b8f8ba7c5787297c687c60d
Hariom-Ingle <hariomingle2003@gmail.com>
2024-04-20 06:41:43 +0530

    Update Background and Font

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
 
   return (
     <TodoProvider value={{todos,addTodo,updateTodo,deleteTodo,toggleComplete}} >
-     <div className="bg-[#19273a] min-h-screen py-8">
+     <div className=" min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-2xl rounded-lg px-4 py-3 text-orange-700   ">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4   text-white ">
